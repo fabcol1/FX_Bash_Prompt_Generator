26b1109171552f71cd862dad3f28d47906c63de3
Fabcol1 <myiscrizioni2@gmail.com>
2017-11-20 14:42:06 +0100

    TextFlow introduction

--- src/bash_prompt_gen/BashPromptGeneratorController.java
+++ src/bash_prompt_gen/BashPromptGeneratorController.java
@@ -1,5 +1,18 @@
 package bash_prompt_gen;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.TextFlow;
+
 public class BashPromptGeneratorController {
+
+	
+	@FXML private TextFlow previewTextFlow;
+	
+	@FXML protected void handleColorPickerPreview(ActionEvent event) {
+		MANAGER.changeTextAreaInnerColor(previewTextFlow, event);
+	}
+
+	private static final BashPromptGeneratorManager MANAGER = new BashPromptGeneratorManager();
 
 }
---
+++ src/bash_prompt_gen/BashPromptGeneratorManager.java
@@ -0,0 +1,31 @@
+package bash_prompt_gen;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextFlow;
+
+public class BashPromptGeneratorManager {
+
+//	style="-fx-control-inner-background:#000000; -fx-font-family: Consolas; -fx-highlight-fill: #00ff00; -fx-highlight-text-fill: #000000; -fx-text-fill: #00ff00; "
+	    
+	/**
+	 * Change TextFlow inner background color
+	 */
+    public void changeTextAreaInnerColor(TextFlow area, ActionEvent event) {
+    	event.consume();
+    	final Color c = ( (ColorPicker) event.getSource()).getValue();
+		area.setStyle("-fx-background-color:" + colorToStringValue(c));
+    }
+    
+    /**
+     * Return #RRGGBB format like String created from parameter Color
+    */
+    private String colorToStringValue(Color c) {
+        int r = (int) (255 * c.getRed());
+        int g = (int) (255 * c.getGreen());
+        int b = (int) (255 * c.getBlue());
+        return String.format("#%02x%02x%02x", r, g, b);
+    }
+	
+}
