ed199e1659d83fe25e00cd9c69b61b1252425356
Fabcol1 <myiscrizioni2@gmail.com>
2017-11-21 12:02:32 +0100

    font size with spinner

--- src/bash_prompt_gen/BashPromptGeneratorController.java
+++ src/bash_prompt_gen/BashPromptGeneratorController.java
@@ -6,24 +6,49 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class BashPromptGeneratorController {
 
-	private final BashPromptGeneratorManager manager = null;
+	private BashPromptGeneratorManager manager;
 	
 	@FXML public TextFlow previewTextFlow;
 	@FXML public ComboBox<String> fontComboBox;
+	@FXML public Spinner<Integer> spinner;
+	
 	
 	@FXML protected void handleColorPickerPreview(ActionEvent event) {
 		if(manager == null) return;
-		manager.changeTextAreaInnerColor(previewTextFlow, event);
+		manager.setTextFlowInnerColor(previewTextFlow, event);
 	}
-
+	
+	@FXML protected void handleFontPickerPreview(ActionEvent event) {
+		if(manager == null) return;
+		manager.setTextFlowFont(previewTextFlow, event);
+	}
+	
+	protected void handleFontSizePreview(Integer newValue) {
+		if(manager == null) return;
+		manager.setTextFlowSize(previewTextFlow, newValue);
+	}
+	
 	public void init() {
-        List<String> families = Font.getFamilies();
-        fontComboBox.setItems(FXCollections.observableList(families));		
+		manager = new BashPromptGeneratorManager();
+		
+		manager.initComboBox(fontComboBox);
+		initSpinnerListener();
+		
+        Text t = new Text("provaprovaprova");
+        previewTextFlow.getChildren().add(t);	
+	}
+	
+	private void initSpinnerListener() {
+		spinner.valueProperty().addListener((obs, oldValue, newValue) -> {
+			handleFontSizePreview(newValue);
+		});
 	}
 	
 }
--- src/bash_prompt_gen/BashPromptGeneratorManager.java
+++ src/bash_prompt_gen/BashPromptGeneratorManager.java
@@ -1,24 +1,80 @@
 package bash_prompt_gen;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextFlow;
 
 public class BashPromptGeneratorManager {
 	
-//	style="-fx-control-inner-background:#000000; -fx-font-family: Consolas; -fx-highlight-fill: #00ff00; -fx-highlight-text-fill: #000000; -fx-text-fill: #00ff00; "
+	/*
+	 * A Map for keep all the styles, avoiding css errors.
+	 */
+	private static final Map<String,String> stylesMap = new HashMap<>();
+
+	
+	//	style="-fx-control-inner-background:#000000; -fx-font-family: Consolas; -fx-highlight-fill: #00ff00; -fx-highlight-text-fill: #000000; -fx-text-fill: #00ff00; "
 	    
-	/**
+	/*
 	 * Change TextFlow inner background color
 	 */
-    public void changeTextAreaInnerColor(TextFlow area, ActionEvent event) {
+	public void setTextFlowInnerColor(TextFlow area, ActionEvent event) {
     	event.consume();
     	final Color c = ( (ColorPicker) event.getSource()).getValue();
-		area.setStyle("-fx-background-color:" + colorToStringValue(c));
+    	stylesMap.put("-fx-background-color:", colorToStringValue(c));
+    	
+    	final StringBuffer sb = new StringBuffer();
+    	stylesMap.forEach( (a,b) -> {
+    		sb.append(a+b+"; ");
+    	});
+    	
+    	area.setStyle(sb.toString());
     }
     
-    /**
+	/*
+	 * Change global TextFlow font family
+	 */
+    public void setTextFlowFont(TextFlow area, ActionEvent event) {
+//        -fx-font-family: "Consolas";
+//    	area.setStyle("-fx-font-family: \"Consolas\"");
+    	
+    	event.consume();
+    	final String s = (String) ((ComboBox<?>) event.getSource()).getValue();
+    	stylesMap.put("-fx-font-family:", s);
+    	
+    	final StringBuffer sb = new StringBuffer();
+    	stylesMap.forEach( (a,b) -> {
+    		sb.append(a+b+"; ");
+    	});
+    	
+    	area.setStyle(sb.toString());
+    }
+
+    
+    /*
+	 * Change global TextFlow font size
+	 */
+    public void setTextFlowSize(TextFlow area, Integer newValue) {
+    	final String s = String.valueOf(newValue);
+    	stylesMap.put("-fx-font-size:", s);
+    	
+    	final StringBuffer sb = new StringBuffer();
+    	stylesMap.forEach( (a,b) -> {
+    		sb.append(a+b+"; ");
+    	});
+    	
+    	area.setStyle(sb.toString());
+    }
+    
+    
+    /*
      * Return #RRGGBB format like String created from parameter Color
     */
     private String colorToStringValue(Color c) {
@@ -27,5 +83,11 @@ public class BashPromptGeneratorManager {
         int b = (int) (255 * c.getBlue());
         return String.format("#%02x%02x%02x", r, g, b);
     }
+
+	public void initComboBox(ComboBox<String> fontComboBox) {
+        final List<String> families = Font.getFamilies();
+        fontComboBox.setItems(FXCollections.observableList(families));	
+        fontComboBox.getSelectionModel().select("Monospaced");
+	}
     
 }
