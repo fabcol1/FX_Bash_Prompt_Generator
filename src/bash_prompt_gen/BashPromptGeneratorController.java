83412c90f581ca0b911da62bad6defa596e37c3e
Fabcol1 <myiscrizioni2@gmail.com>
2017-11-22 15:26:17 +0100

    add colors

--- src/bash_prompt_gen/BashPromptGeneratorController.java
+++ src/bash_prompt_gen/BashPromptGeneratorController.java
@@ -6,7 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -19,6 +22,8 @@ public class BashPromptGeneratorController {
 	@FXML public ComboBox<String> fontComboBox;
 	@FXML public Spinner<Integer> spinner;
 	
+	@FXML public ToggleGroup toggleGroup1;
+	@FXML public RadioButton redRadio;
 	
 	@FXML protected void handleColorPickerPreview(ActionEvent event) {
 		if(manager == null) return;
@@ -37,6 +42,14 @@ public class BashPromptGeneratorController {
 	
 	public void init() {
 		manager = new BashPromptGeneratorManager();
+		
+//		redRadio.getStyleClass().remove("radio-button");
+//		redRadio.getStyleClass().add("toggle-button");
+//		
+//		toggleGroup1.getToggles().forEach(t -> {
+//			((ToggleButton) t).getStyleClass().remove("radio-button");
+//			((ToggleButton) t).getStyleClass().add("toggle-button");
+//		});
 		
 		manager.initComboBox(fontComboBox);
 		initSpinnerListener();
